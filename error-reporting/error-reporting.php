bd68b9edb5a0ef0b5961335beec4a7cae812847a
Kanem <kanemdeng@email.com>
2024-01-11 13:01:00 -0800

    set error reporting on browser

---
+++ error-reporting/error-reporting.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Title</h1>
+  <?php
+  $name = 'John';
+  ?>
+  <p>
+    <?php
+
+    echo 'Hello, ' . $name; ?></p>
+  <p><?php
+      echo "Hey, $name; ?></p>
+</body>
+
+</html>

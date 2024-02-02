6a3183f07706cba4b6ddb76b9c7892f59257ffad
Kanem <kanemdeng@email.com>
2024-02-01 18:00:33 -0800

    connecting to sql database

--- project/app/config.php
+++ project/app/config.php
@@ -1,9 +1,3 @@
 <?php
 
-const CONFIG = [
-  'data_file' => APP_PATH . 'data.json',
-  'db' => 'postgreSQL:dbname=php_glossary;host=localhost;port=5432',
-  'users' => [
-    'admin@admin.com' => 'abcd'
-  ]
-];
+require('../../config.php');
